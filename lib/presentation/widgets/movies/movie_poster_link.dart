7857b8444198f8cbf44a6edf6a3ace0998aba6b5
sebastianm7 <juansebastianmame@ufps.edu.co>
2023-12-17 15:09:20 -0500

    Se creo home view, movie poster link y full screen loader

--- lib/presentation/widgets/movies/movie_poster_link.dart
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -1,1 +1,23 @@
-// TODO: Sebastian crear movie poster link
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia_movil/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MoviePosterLink extends StatelessWidget {
+  final Movie movie;
+
+  const MoviePosterLink({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInUp(
+      child: GestureDetector(
+        onTap: () => context.push('/home/0/movie/${movie.id}'),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(movie.posterPath),
+        ),
+      ),
+    );
+  }
+}
