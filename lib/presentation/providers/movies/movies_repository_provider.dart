b3a555921f5664f5b4b5ad4f216d1c89fb7e0391
k@miloJ <kamilo_1205@outlook.com>
2023-12-19 06:12:17 -0500

    corregir y actualizar librerias

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:cinemapedia_movil/infrastructure/datasource/moviedb_datasource.dart';
+import 'package:cinemapedia_movil/infrastructure/datasources/moviedb_datasource.dart';
 import 'package:cinemapedia_movil/infrastructure/repositories/movies_repository_impl.dart';
 
 final movieRepositoryProvider = Provider((ref) {
